923d39a6226ab2d3c6c26a409b64bb4a657ee6a3
Mal <malvasan@gmail.com>
2020-10-22 11:28:18 -0700

    test fix

--- app/consumers/users_consumer.rb
+++ app/consumers/users_consumer.rb
@@ -4,8 +4,7 @@ class UsersConsumer < ApplicationConsumer
     def consume 
         params_batch.each do |message|
             puts "Message payload: #{message.payload}"
-            user = User.new
-            user_decoded = user.decode(message.payload)
+            user_decoded = User.decode(message.payload)
             puts "Message decoded" 
           end
     end
